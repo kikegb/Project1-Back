a191d4e2e887b93fabf3637df4e55bac6ac2c65f
Enrique Gómez <57214961+kikegb@users.noreply.github.com>
2022-11-11 12:21:26 +0100

    User service tested

--- src/test/java/com/enrique/project1back/service/UserServiceTest.java
+++ src/test/java/com/enrique/project1back/service/UserServiceTest.java
@@ -17,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -60,9 +61,10 @@ public class UserServiceTest {
     @BeforeEach
     public void setUpMock() {
         Mockito.lenient().when(userRepository.insert(any(User.class))).then(AdditionalAnswers.returnsFirstArg());
+        Mockito.lenient().when(userRepository.save(any(User.class))).then(AdditionalAnswers.returnsFirstArg());
     }
 
-    @DisplayName("Test add user")
+    @DisplayName("Test add user: Success")
     @Test
     public void testAdd() {
         User user = userList.get(0);
@@ -73,7 +75,7 @@ public class UserServiceTest {
         verify(userRepository).insert(user);
     }
 
-    @DisplayName("Test add invalid user")
+    @DisplayName("Test add user: Existent email error")
     @Test
     public void testAddInvalid() {
         User user = userList.get(1);
@@ -81,9 +83,10 @@ public class UserServiceTest {
 
         assertNull(userService.add(user));
         verify(userRepository).existsByEmail(user.getEmail());
+        verifyNoMoreInteractions(userRepository);
     }
 
-    @DisplayName("Test find by email")
+    @DisplayName("Test find by email: Success")
     @Test
     public void testFindByEmail() {
         User user = userList.get(2);
@@ -93,7 +96,7 @@ public class UserServiceTest {
         verify(userRepository).findUserByEmail(user.getEmail());
     }
 
-    @DisplayName("Test find by email")
+    @DisplayName("Test find by email: Email not exists error")
     @Test
     public void testFindByNonExistentEmail() {
         User user = userList.get(3);
@@ -103,7 +106,7 @@ public class UserServiceTest {
         verify(userRepository).findUserByEmail(user.getEmail());
     }
 
-    @DisplayName("Test find by id")
+    @DisplayName("Test find by id: Success")
     @Test
     public void testFindById() {
         User user = userList.get(0);
@@ -113,7 +116,7 @@ public class UserServiceTest {
         verify(userRepository).findById(user.getId());
     }
 
-    @DisplayName("Test find by invalid id")
+    @DisplayName("Test find by id: Id not exists error")
     @Test
     public void testFindByInvalidId() {
         User user = userList.get(1);
@@ -121,5 +124,100 @@ public class UserServiceTest {
 
         assertNull(userService.findById(user.getId()));
         verify(userRepository).findById(user.getId());
+    }
+
+    @DisplayName("Test find all: Success")
+    @Test
+    public void testFindAll() {
+        when(userRepository.findAll()).thenReturn(userList);
+
+        List<User> result = userService.findAll();
+        assertEquals(4, result.size());
+        assertEquals(userList, result);
+        verify(userRepository).findAll();
+    }
+
+    @DisplayName("Test find all: Success empty")
+    @Test
+    public void testFindAllEmpty() {
+        when(userRepository.findAll()).thenReturn(new ArrayList<>());
+
+        List<User> result = userService.findAll();
+        assertEquals(0, result.size());
+        assertEquals(List.of(), result);
+        verify(userRepository).findAll();
+    }
+
+    @DisplayName("Test update name: Success")
+    @Test
+    public void testUpdateName() {
+        User user = userList.get(2);
+        User updatedUser = new User(
+                user.getId(),
+                "Susana Torio",
+                user.getPassword(),
+                user.getEmail(),
+                user.getBirthday()
+        );
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        User result = userService.updateName(user.getId(), updatedUser.getName());
+        assertEquals(updatedUser, result);
+        verify(userRepository).findById(user.getId());
+        verify(userRepository).save(user);
+    }
+
+    @DisplayName("Test update name: Id not exists error")
+    @Test
+    public void testUpdateNameNonExistentId() {
+        User user = userList.get(3);
+        User updatedUser = new User(
+                user.getId(),
+                "Susana Torio",
+                user.getPassword(),
+                user.getEmail(),
+                user.getBirthday()
+        );
+        when(userRepository.findById(user.getId())).thenReturn(Optional.empty());
+
+        assertNull(userService.updateName(user.getId(), updatedUser.getName()));
+        verify(userRepository).findById(user.getId());
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @DisplayName("Test find by id: Success")
+    @Test
+    public void testDeleteById() {
+        User user = userList.get(0);
+        when(userRepository.deleteUserById(user.getId())).thenReturn(1);
+
+        assertEquals(1, userService.deleteById(user.getId()));
+        verify(userRepository).deleteUserById(user.getId());
+    }
+
+    @DisplayName("Test find by id: Id not exists error")
+    @Test
+    public void testDeleteByNonExistentId() {
+        User user = userList.get(0);
+        when(userRepository.deleteUserById(user.getId())).thenReturn(0);
+
+        assertEquals(0, userService.deleteById(user.getId()));
+        verify(userRepository).deleteUserById(user.getId());
+    }
+
+    @DisplayName("Test update: Success")
+    @Test
+    public void testUpdate() {
+        User user = userList.get(3);
+        User updatedUser = new User(
+                user.getId(),
+                "Susana Torio",
+                "storio84",
+                "storio@gmail.com",
+                LocalDate.of(1999, 8, 23)
+        );
+
+        assertEquals(updatedUser, userService.update(updatedUser));
+        verify(userRepository).save(updatedUser);
     }
 }
